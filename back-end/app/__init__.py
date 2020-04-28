ae1a6d39dd9bf1729c17754ee7781925cd82ac27
yyx <y330266606@gmail.com>
2020-04-28 02:54:01 +0200

    Flask design APIs of User

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from config import Config
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# Flask-SQLAlchemy plugin
+db = SQLAlchemy()
+# Flask-Migrate plugin
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
@@ -8,8 +15,13 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     CORS(app)
-    
+    # init flask_mongoengine
+    db.init_app(app)
+    # init flask_migrate
+    migrate.init_app(app, db)
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
     return app
+
+from app import models
